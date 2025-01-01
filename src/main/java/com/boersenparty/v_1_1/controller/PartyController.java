5d22ea16f966d6dad757cb83d059b62ffe95b5e0
mfnomad <mfnomad@gmail.com>
2025-01-01 18:02:40 +0100

    getParties() access between Veranstalter / Personaler mapping of : Personaler -> entsprechender Veranstalter

--- src/main/java/com/boersenparty/v_1_1/controller/PartyController.java
+++ src/main/java/com/boersenparty/v_1_1/controller/PartyController.java
@@ -4,6 +4,7 @@ import com.boersenparty.v_1_1.models.Party;
 import com.boersenparty.v_1_1.service.PartyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,7 @@ public class PartyController implements PartyControllerInterface {
             org.springframework.security.oauth2.jwt.Jwt jwt = (org.springframework.security.oauth2.jwt.Jwt) principal;
 
             String name = jwt.getClaim("preferred_username");
+
             if (name != null) {
                 return name;
             }
@@ -38,12 +40,62 @@ public class PartyController implements PartyControllerInterface {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
-    @Override
-    @PreAuthorize("hasAuthority('PARTIES_READ')")
-    public List<Party> getParties() {
-        System.out.println("getParties() called");
-        return partyService.getPartiesHostedBy(getUserID());
+    private boolean hasAuthority(String authority) {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(authority));
     }
+
+    private String getWorksForFromToken() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof org.springframework.security.oauth2.jwt.Jwt) {
+            org.springframework.security.oauth2.jwt.Jwt jwt = (org.springframework.security.oauth2.jwt.Jwt) principal;
+            return jwt.getClaim("works_for");
+        }
+
+        // Fallback if the attribute is missing or token is invalid
+        return null;
+    }
+
+
+
+    /*
+    Ein 'Veranstalter' braucht bloß die '_VERANSTALTER' Rolle um getParties() zu betätigen
+
+    Ein 'Personaler' jedoch braucht die '_PERSONAL' Rolle UND das Custom Attribute 'works_for'
+    muss in Keycloak gesetzt werden -> sodass ein entsprechendes Mapping zwischen den Arbeitern
+    eines Veranstalters möglich ist
+    z.b. 'works_for' = 'veranstalter' /
+    'works_for' = 'veranstalter2'
+
+     */
+
+    @Override
+    @PreAuthorize("hasAnyAuthority('_VERANSTALTER', '_PERSONAL')")
+    public List<Party> getParties() {
+        System.out.println("getParties is called in controller!");
+        if (hasAuthority("_VERANSTALTER")) {
+            System.out.println("Authority: _VERANSTALTER!!!!!!!!!!");
+            return partyService.getPartiesHostedBy(getUserID());
+        } else if (hasAuthority("_PERSONAL")) {
+            System.out.println("Authority: _PERSONAL!!!!!!!!!!");
+
+            String veranstalterId = getWorksForFromToken();
+            System.out.println("works for is:" + veranstalterId);
+            if (veranstalterId == null || veranstalterId.isEmpty()) {
+                throw new AccessDeniedException("Personal user is not associated with any Veranstalter.");
+            }
+
+            return partyService.getPartiesHostedBy(veranstalterId);
+        }
+
+        else {
+            throw new AccessDeniedException("User does not have access to any parties.");
+        }
+
+    }
+
+
 
     @Override
     public Optional<Party> getParty(Long party_id) {
