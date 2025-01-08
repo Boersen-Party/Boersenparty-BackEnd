91b31d5c2c7e5bbd8b2b557c5818321c0ffd2f6a
Dominik <147036891+doeiit00@users.noreply.github.com>
2025-01-08 22:07:05 +0100

    Update MyConfiguration.java

--- src/main/java/com/boersenparty/v_1_1/config/MyConfiguration.java
+++ src/main/java/com/boersenparty/v_1_1/config/MyConfiguration.java
@@ -11,6 +11,6 @@ public class MyConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
     }
 }
