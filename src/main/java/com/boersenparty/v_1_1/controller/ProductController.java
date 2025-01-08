3e10dcc9f86479cb88a41980258ced488074904e
Dominik <147036891+doeiit00@users.noreply.github.com>
2025-01-08 22:16:19 +0100

    CrossOrigin
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

--- src/main/java/com/boersenparty/v_1_1/controller/ProductController.java
+++ src/main/java/com/boersenparty/v_1_1/controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class ProductController implements ProductControllerInterface {
 
--- src/main/java/com/boersenparty/v_1_1/controller/QRCodeController.java
+++ src/main/java/com/boersenparty/v_1_1/controller/QRCodeController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Base64;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class QRCodeController {
 
